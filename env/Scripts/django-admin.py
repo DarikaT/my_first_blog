0b7c394399c3fd5e7979d9972f71844f420b7f13
darika <darikman99@gmail.com>
2020-01-17 04:20:12 +0600

    My Django Girls app, first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\darik\onedrive\рабочий стол\djangogirls\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
